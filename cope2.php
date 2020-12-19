0001bd224163fc8f3291fb3b062cc5f1fc843cde
atgargentina <76268732+atgargentina@users.noreply.github.com>
2020-12-19 20:58:28 -0300

    Add files via upload
    primera subida

---
+++ cope2.php
@@ -0,0 +1,36 @@
+<?php
+if(isset($_POST['email'])) {
+
+<bold>// Debes editar las próximas dos líneas de código de acuerdo con tus preferencias</bold>
+$email_to = "lisandroburgues@yahoo.com.ar";//cambiar el correo cuando esto este andando
+$email_subject = "Contacto de una persona interesada en unirse a la cooperativa";
+
+<bold>// Aquí se deberían validar los datos ingresados por el usuario</bold>
+if(!isset($_POST['name']) ||
+!isset($_POST['email']) ||
+!isset($_POST['subject']) ||
+!isset($_POST['message']) ||
+
+echo "<b>Ocurrió un error y el formulario no ha sido enviado. </b><br />";
+echo "Por favor, vuelva atrás y verifique la información ingresada<br />";
+die();
+}
+
+$email_message = "Detalles del formulario de contacto:\n\n";
+$email_message .= "Nombre: " . $_POST['name'] . "\n";
+$email_message .= "Email: " . $_POST['email'] . "\n";
+$email_message .= "Asunto: " . $_POST['subject'] . "\n";
+$email_message .= "Mensaje: " . $_POST['message'] . "\n";
+
+
+<bold>// Ahora se envía el e-mail usando la función mail() de PHP</bold>
+$headers = 'From: '.$email_from."\r\n".
+'Reply-To: '.$email_from."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+@mail($email_to, $email_subject, $email_message, $headers);
+
+echo "¡El formulario se ha enviado con éxito!";
+}
+headers("Location:index.html");
+
+?>
